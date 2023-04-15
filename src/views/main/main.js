990b36cbc291294e9b09cc500f75e1920f311683
ministrov <61511030+ministrov@users.noreply.github.com>
2023-04-15 17:07:07 +0300

    feat: create main view

---
+++ src/views/main/main.js
@@ -0,0 +1,14 @@
+import { AbstractView } from "../../common/view";
+
+export class MainView extends AbstractView {
+  constructor() {
+    super();
+    this.setTitle('Поиск книг');
+  }
+
+  render() {
+    const main = document.createElement('div');
+    main.innerHTML = 'Тест';
+    this.app.append(main);
+  }
+}
